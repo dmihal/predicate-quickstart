0dd52d85a2390c06b9467692ca6b1930c8eb6f2b
David Mihal <david@dmihal.com>
2023-11-18 12:09:00 +0300

    Predicate for tranfering to recipient

--- simple-predicate/src/main.sw
+++ simple-predicate/src/main.sw
@@ -1,5 +1,32 @@
 predicate;
 
+use std::{
+    constants::ZERO_B256,
+    outputs::{
+        output_asset_to,
+        output_count,
+    },
+};
+
+configurable {
+    SENDER: b256 = ZERO_B256,
+    RECIPIENT: b256 = ZERO_B256,
+}
+
 fn main() -> bool {
+    let count = output_count();
+    let mut i = 0;
+
+    while i < count {
+        match output_asset_to(i) {
+            Some(recipient) => {
+                if recipient != RECIPIENT && recipient != SENDER {
+                    return false;
+                }
+            },
+            None => (),
+        }
+        i += 1;
+    }
     return true;
 }
--- simple-predicate/tests/harness.rs
+++ simple-predicate/tests/harness.rs
@@ -1,4 +1,4 @@
-use fuels::{accounts::predicate::Predicate, prelude::*};
+use fuels::{accounts::predicate::Predicate, types::Bits256, prelude::*};
 
 // Load abi from json
 abigen!(Predicate(
@@ -6,7 +6,7 @@ abigen!(Predicate(
     abi = "out/debug/simple_predicate-abi.json"
 ));
 
-async fn get_wallets() -> (WalletUnlocked, WalletUnlocked) {
+async fn get_wallets() -> (WalletUnlocked, WalletUnlocked, WalletUnlocked) {
     // Launch a local network and deploy the contract
     let mut wallets = launch_custom_provider_and_get_wallets(
         WalletsConfig::new(
@@ -21,11 +21,14 @@ async fn get_wallets() -> (WalletUnlocked, WalletUnlocked) {
 
     let wallet_1 = wallets.pop().unwrap();
     let wallet_2 = wallets.pop().unwrap();
-    (wallet_1, wallet_2)
+    let wallet_3 = wallets.pop().unwrap();
+    (wallet_1, wallet_2, wallet_3)
 }
 
-fn get_predicate(provider: &Provider) -> Predicate {
-    let configurables = SimplePredicateConfigurables::new();
+fn get_predicate(sender: &Bech32Address, recipient: &Bech32Address, provider: &Provider) -> Predicate {
+    let configurables = SimplePredicateConfigurables::new()
+        .with_SENDER(Bits256(sender.hash().into()))
+        .with_RECIPIENT(Bits256(recipient.hash().into()));
 
     let mut predicate: Predicate = Predicate::load_from("out/debug/simple_predicate.bin")
         .unwrap()
@@ -37,9 +40,9 @@ fn get_predicate(provider: &Provider) -> Predicate {
 
 #[tokio::test]
 async fn can_send() {
-    let (wallet_1, wallet_2) = get_wallets().await;
+    let (wallet_1, wallet_2, _wallet_3) = get_wallets().await;
     let provider = wallet_1.provider().unwrap();
-    let predicate = get_predicate(&provider);
+    let predicate = get_predicate(&wallet_1.address(), &wallet_2.address(), &provider);
 
     let amount = 1_000;
 
@@ -64,4 +67,68 @@ async fn can_send() {
         )
         .await
         .unwrap();
+}
+
+#[tokio::test]
+async fn can_send_back() {
+    let (wallet_1, wallet_2, _wallet_3) = get_wallets().await;
+    let provider = wallet_1.provider().unwrap();
+    let predicate = get_predicate(&wallet_1.address(), &wallet_2.address(), &provider);
+
+    let amount = 1_000;
+
+    // Fund the predicate
+    wallet_1
+        .transfer(
+            predicate.address(),
+            amount,
+            BASE_ASSET_ID,
+            TxParameters::default(),
+        )
+        .await
+        .unwrap();
+
+    // Send a transfer
+    predicate
+        .transfer(
+            wallet_1.address(),
+            amount,
+            BASE_ASSET_ID,
+            TxParameters::default(),
+        )
+        .await
+        .unwrap();
+}
+
+#[tokio::test]
+async fn shouldnt_send_to_random_person() {
+    let (wallet_1, wallet_2, wallet_3) = get_wallets().await;
+    let provider = wallet_1.provider().unwrap();
+    let predicate = get_predicate(&wallet_1.address(), &wallet_2.address(), &provider);
+
+    let amount = 1_000;
+
+    // Fund the predicate
+    wallet_1
+        .transfer(
+            predicate.address(),
+            amount,
+            BASE_ASSET_ID,
+            TxParameters::default(),
+        )
+        .await
+        .unwrap();
+
+    // Send a transfer
+    let tx = predicate
+        .transfer(
+            wallet_3.address(),
+            amount,
+            BASE_ASSET_ID,
+            TxParameters::default(),
+        )
+        .await;
+
+    let is_err = tx.is_err();
+    assert!(is_err);
 }
